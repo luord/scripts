b1875146b6960a09aa03111cad1ed911c373a5f2
Luis Orduz <lo@luord.com>
2024-12-20 19:29:01 -0500

    cleaned up and changed install order

--- setup.sh
+++ setup.sh
@@ -4,18 +4,19 @@ set -ex -o pipefail
 CONFIG_DIR=${CONFIG_DIR:-$HOME/Documentos/config}
 
 sudo systemctl mask \
-  sleep.target suspend.target hibernate.target hybrid-sleep.target
+ sleep.target suspend.target hibernate.target hybrid-sleep.target
 
 sudo sed -i 's/trixie/testing/g' /etc/apt/sources.list
 sudo apt update
 sudo apt dist-upgrade -y
 
-sudo apt install --no-install-recommends \
-  gdm3 gnome-session gnome-shell gnome-terminal gnome-music totem
-
 sudo apt install \
-  gnome-shell-extension-prefs transmission-gtk rclone curl \
-  python3-venv nodejs neovim git make shellcheck
+ gnome-music totem transmission-gtk \
+ rclone curl \
+ python3-venv nodejs neovim git make shellcheck
+
+sudo apt install --no-install-recommends \
+ gdm3 gnome-session gnome-shell gnome-terminal
 
 _tailscale () {
   if command -v tailscale; then
@@ -32,7 +33,8 @@ _nvim () {
   if [[ -d ~/.config/nvim/pack/github/start/copilot.vim ]]; then
     printf "already set up\n"
   else
-    git clone https://github.com/github/copilot.vim.git ~/.config/nvim/pack/github/start/copilot.vim
+    git clone https://github.com/github/copilot.vim.git \
+     ~/.config/nvim/pack/github/start/copilot.vim
     nvim --headless +"Copilot setup" +q
   fi
 }
@@ -43,8 +45,11 @@ _chrome () {
   if command -v google-chrome; then
     printf "already installed\n"
   else
-    curl -fSsL https://dl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor | sudo tee /usr/share/keyrings/google-chrome.gpg >> /dev/null
-    echo deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main | sudo tee /etc/apt/sources.list.d/google-chrome.list
+    curl -fSsL https://dl.google.com/linux/linux_signing_key.pub \
+     | sudo gpg --dearmor \
+     | sudo tee /usr/share/keyrings/google-chrome.gpg >> /dev/null
+    echo "deb http://dl.google.com/linux/chrome/deb/ stable main" \
+     | sudo tee /etc/apt/sources.list.d/google-chrome.list
     sudo apt update
     sudo apt install google-chrome-stable 
   fi
@@ -55,7 +60,8 @@ _chrome
 _nft () {
   sudo cp "$CONFIG_DIR/nftables.conf" /etc/nftables.conf
   sudo systemctl enable nftables
-  printf "net.ipv4.ip_forward=1" | sudo tee /etc/sysctl.d/95-IPv4-forwarding.conf
+  printf "net.ipv4.ip_forward=1" \
+   | sudo tee /etc/sysctl.d/95-IPv4-forwarding.conf
   sudo sysctl -p /etc/sysctl.d/95-IPv4-forwarding.conf
 }
 
@@ -67,10 +73,14 @@ _rclone () {
   else
     mkdir -p ~/.config/rclone
     cp "$CONFIG_DIR/rclone.conf" ~/.config/rclone/rclone.conf
-    rclone --filter "- .**" bisync ~/Música remote:Music --resync --remove-empty-dirs
-    rclone --filter "- .**" bisync ~/Vídeos remote:Videos --resync --remove-empty-dirs
-    rclone --filter "- .**" bisync ~/Imágenes remote:Pictures --resync --remove-empty-dirs
-    rclone --filter "- .**" bisync ~/Documentos remote:Documents --resync --remove-empty-dirs
+    rclone --filter "- .**" bisync \
+     ~/Música remote:Music --resync --remove-empty-dirs
+    rclone --filter "- .**" bisync \
+     ~/Vídeos remote:Videos --resync --remove-empty-dirs
+    rclone --filter "- .**" bisync \
+     ~/Imágenes remote:Pictures --resync --remove-empty-dirs
+    rclone --filter "- .**" bisync \
+     ~/Documentos remote:Documents --resync --remove-empty-dirs
   fi
 }
 
@@ -91,12 +101,12 @@ _docker () {
     printf "already installed\n"
   else
     sudo install -m 0755 -d /etc/apt/keyrings
-    sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+    sudo curl -fsSL https://download.docker.com/linux/debian/gpg \
+     -o /etc/apt/keyrings/docker.asc
     sudo chmod a+r /etc/apt/keyrings/docker.asc
     echo \
-      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-      bookworm stable" | \
-      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+     "deb https://download.docker.com/linux/debian bookworm stable" \
+     | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt update
     sudo apt install docker-ce uidmap
     sudo systemctl disable --now docker.service docker.socket
